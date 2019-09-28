353f44a0d14c00c291eb3bbc1c76d7ed1e6707a5
Seunghyun Kim <kimseunghyun.bg@gmail.com>
2019-09-28 12:59:45 +0900

    solve Cut the sticks
    Signed-off-by: Seunghyun Kim <kimseunghyun.bg@gmail.com>

---
+++ src/main/java/hackerrank/algorithm/easy/CutTheSticks.java
@@ -0,0 +1,65 @@
+package hackerrank.algorithm.easy;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+public class CutTheSticks {
+
+    // Complete the cutTheSticks function below.
+    static int[] cutTheSticks(int[] arr) {
+        List<Integer> sticksCuts = new ArrayList<>();
+        Map<Integer, Integer> inputMap = new TreeMap<>(Comparator.naturalOrder());
+        for (int i : arr) {
+            if (inputMap.get(i) == null){
+                inputMap.put(i,1);
+            }else {
+                inputMap.put(i,inputMap.get(i)+1);
+            }
+        }
+
+        int len = arr.length;
+        for (Integer i : inputMap.keySet()) {
+            sticksCuts.add(len);
+            len -= inputMap.get(i);
+        }
+
+        return sticksCuts.stream().mapToInt(i -> i).toArray();
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int[] result = cutTheSticks(arr);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
---
+++ src/test/java/hackerrank/algorithm/easy/CutTheSticksTest.java
@@ -0,0 +1,29 @@
+package hackerrank.algorithm.easy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class CutTheSticksTest {
+
+    static Stream<Arguments> testCutTheSticks() {
+        return Stream.of(
+                Arguments.of(new int[]{5, 4, 4, 2, 2, 8}, new int[]{6, 4, 2, 1})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testCutTheSticks(int[] arr, int[] expected) {
+        // given
+        // when
+        int[] actual = CutTheSticks.cutTheSticks(arr);
+        // then
+        assertThat(actual, is(expected));
+    }
+}
