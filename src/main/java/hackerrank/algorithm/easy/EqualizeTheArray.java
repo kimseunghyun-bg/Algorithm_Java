2df9783002b947a7f24e4be7b1a001abdafd6cfd
Seunghyun Kim <kimseunghyun.bg@gmail.com>
2019-10-03 00:47:59 +0900

    solve Equalize the Array
    Signed-off-by: Seunghyun Kim <kimseunghyun.bg@gmail.com>

---
+++ src/main/java/hackerrank/algorithm/easy/EqualizeTheArray.java
@@ -0,0 +1,60 @@
+package hackerrank.algorithm.easy;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+
+public class EqualizeTheArray {
+
+    // Complete the equalizeArray function below.
+    static int equalizeArray(int[] arr) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (countMap.get(arr[i]) != null) {
+                countMap.put(arr[i], countMap.get(arr[i]) + 1);
+            } else {
+                countMap.put(arr[i], 1);
+            }
+        }
+
+        int max = 0;
+        for (int i : countMap.keySet()) {
+            if (max < countMap.get(i)) {
+                max = countMap.get(i);
+            }
+        }
+        return arr.length-max;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = equalizeArray(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
--- src/test/java/hackerrank/algorithm/easy/EqualizeTheArrayTest.java
+++ src/test/java/hackerrank/algorithm/easy/EqualizeTheArrayTest.java
@@ -9,21 +9,20 @@ import java.util.stream.Stream;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-class ElectronicsShopTest {
+class EqualizeTheArrayTest {
 
-    static Stream<Arguments> testGetMoneySpent() {
+    static Stream<Arguments> testEqualizeArray() {
         return Stream.of(
-                Arguments.of(new int[]{3, 1}, new int[]{5, 2, 8}, 10, 9),
-                Arguments.of(new int[]{5}, new int[]{4}, 5, -1)
+                Arguments.of(new int[]{3, 3, 2, 1, 3}, 2)
         );
     }
 
     @ParameterizedTest
     @MethodSource
-    void testGetMoneySpent(int[] keyboards, int[] drives, int b, int expected) {
+    void testEqualizeArray(int[] arr, int expected) {
         // given
         // when
-        int actual = ElectronicsShop.getMoneySpent(keyboards, drives, b);
+        int actual = EqualizeTheArray.equalizeArray(arr);
         // then
         assertThat(actual, is(expected));
     }
